7e6b05f5288256de4242e2aa61ddb89297c19214
bluebearrii <thananan.math.acs@gmail.com>
2020-08-28 00:35:03 +0700

    :tada: Dart language || Declare variable

---
+++ Variable.dart
@@ -0,0 +1,15 @@
+void main() {
+  run();
+}
+
+void run(){
+
+  // Use var for declare variable & can use all type like JAVA and JavaScript 📓
+  // [int, float, string array object]  💯
+  var name = "admin";
+  var lastName = "nano";
+  print(name);
+  print(lastName);
+  print(name + lastName);
+  print('$name $lastName');
+}
